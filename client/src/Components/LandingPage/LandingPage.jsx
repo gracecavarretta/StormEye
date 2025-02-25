fabb65ea56c8a6ec7f3b7063193e63a68976db15
Grace Cavarretta <gvcavarretta@gmail.com>
2025-02-25 09:25:46 -0500

    Added landing page/login/signup

---
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="logo">StormEye</div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login/Sign Up</Link></li>
+        </ul>
+      </nav>
+
+      <div className="hero">
+        <h1>Welcome to StormEye</h1>
+        <Link to="/login" className="btn">Login/Sign Up</Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;  
