054bd5557b2f12847850156f7f5829473b1f4054
noranguy <148975883+noranguy@users.noreply.github.com>
2025-02-27 22:23:27 -0500

    made another folder for repeated css components (navbar)

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import MainImage from "../../assets/images/LandingPageMainImage.jpg";
 import "./LandingPage.css";
+import "../RepeatedCSS/NavBar.css";
 
 const LandingPage = () => {
   return (
