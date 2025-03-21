c03d1e90f07a8de9e138de11ac672769a6fe0808
noranguy <148975883+noranguy@users.noreply.github.com>
2025-03-21 17:43:51 -0400

    added intros to the relief page, added main image for h1

--- client/src/Components/ReliefInfoPage/ReliefPage.jsx
+++ client/src/Components/ReliefInfoPage/ReliefPage.jsx
@@ -2,28 +2,42 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./ReliefPage.css";
 import NavBar from '../NavBar/NavBar.jsx'
-
+import MainImage from "../../assets/images/ReliefPageMainImage.jpg";
+//
 const ReliefPage = () => {
   return (
     <div>
       <NavBar />
       <div className="h-relief">
-        <h1>Relief Information</h1>
+        <div class="h-relief-image-box">
+          <img src={MainImage}/>
+          <h1>Relief Information</h1>
+        </div>
       </div>
       <div className="relief-container">
         <div className="relief-box">
           <h2>Before a Disaster</h2>
-          <p>blah blah blah</p>
+          <p>Preparation is key to staying safe in a storm. <br></br><br></br>
+            Taking action before the storm arrives can make all the difference in 
+            protecting your home, securing essential supplies, and ensuring your 
+            family is ready for any emergency. </p>
           <Link to="/relief-before" className="relief-btn">Learn More</Link>
         </div>
         <div className="relief-box">
           <h2>During a Disaster</h2>
-          <p>blah blah blah</p>
+          <p>As a hurricane arrives, your priority is to stay 
+            safe. <br></br><br></br>
+            High winds, heavy rain, and potential flooding can create 
+            life-threatening conditions. It is incredibly crucial to follow emergency guidelines,
+            find shelter, and seek evacuation. </p>
           <Link to="/relief-during" className="relief-btn">Learn More</Link>
         </div>
         <div className="relief-box">
           <h2>After a Disaster</h2>
-          <p>blah blah blah</p>
+          <p>The storm passed, but the danger isn’t over yet.  <br></br><br></br>
+             After a hurricane, there numerous hazards that pose high risks.
+             As you assess the aftermath, it’s crucial to take precautions, 
+             follow local emergency guidance, and access available relief resources.</p>
           <Link to="/relief-after" className="relief-btn">Learn More</Link>
         </div>
       </div>
